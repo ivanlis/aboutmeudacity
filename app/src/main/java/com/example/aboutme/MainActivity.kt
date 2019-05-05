c2aacb5c563841db6487eb5c1cbe21b566c17d3e
Ivan Lysiuchenko <ivanlis.bilbao@gmail.com>
2019-05-05 14:13:56 +0200

    Delete some commented out code.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -4,12 +4,8 @@ import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import com.example.aboutme.databinding.ActivityMainBinding
 import com.example.android.aboutme.MyName
 
@@ -31,20 +27,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNickname(view: View) {
-//        val editText = binding.nicknameEdit
-//        val nicknameTextView = binding.nicknameText
-//
-//
-//        //nicknameTextView.text = editText.text
-//        myName?.nickname = editText.text.toString()
-//        //invalidateAll()
-//        editText.visibility = View.GONE
-//        view.visibility = View.GONE
-//        nicknameTextView.visibility = View.VISIBLE
-//
-//        // Hide the keyboard.
-//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view.windowToken, 0)
 
         binding.apply {
             myName?.nickname = nicknameEdit.text.toString()
